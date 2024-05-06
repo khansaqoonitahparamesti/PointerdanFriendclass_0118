c56b07643183fad6c5aed7c6545e30cd9f2aef28
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-06 11:21:04 +0700

    masukkan class

---
+++ FriendFunction2/FriendFunction2.cpp
@@ -0,0 +1,17 @@
+// FriendFunction2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class pelajar;
+class manusia {
+
+public:
+	void showNilaiPelajar(pelajar& x);
+};
+
+class pelajar
+{
+private:
+	int nilai;
+};
